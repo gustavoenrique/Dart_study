bcf6282893e8ee281cdc50c8e6b2813e658cb691
Gustavo Enrique Mariano <d3v.guss@gmail.com>
2021-09-14 23:45:53 -0300

    OO Dart Study

--- oo/teste/teste_carro.dart
+++ oo/teste/teste_carro.dart
@@ -4,13 +4,13 @@ main() {
   var c1 = Carro(55);
 
   while (!c1.estaNoLimite()) {
-    print("Acelerando o carro. Velocidade de: ${c1.Acelerar()} Km/h");
+    print("Acelerando o carro. Velocidade de: ${c1.acelerar()} Km/h");
   }
 
   print("Velocidade máxima de ${c1.velocidadeAtual} Km/h");
 
   while (c1.velocidadeAtual > 0) {
-    print("Freando o carro. Velocidade de: ${c1.Frear()} Km/h");
+    print("Freando o carro. Velocidade de: ${c1.frear()} Km/h");
   }
 
   print("O carro parou na velocidade de ${c1.velocidadeAtual} Km/h");
